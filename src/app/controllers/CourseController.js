7e2e1982430e571ee7b185eff3153bed849051f5
baotheo1999 <nguyenquanganhbao148@gmail.com>
2021-09-03 20:44:42 +0700

    bài 27

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -11,6 +11,21 @@ class CourseController {
 			})
 			.catch(next);
 	}
+	// [GET] /courses/create
+	create(req, res, next) {
+		res.render('courses/create');
+	}
+
+	// [POST] /courses/create
+	store(req, res, next) {
+		const formData = req.body;
+		formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+		const course = new Course(formData);
+		course
+			.save()
+			.then(() => res.redirect('/'))
+			.catch((err) => {});
+	}
 }
 
 module.exports = new CourseController();
--- src/resource/routes/courses.js
+++ src/resource/routes/courses.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const conrseContrller = require('../../app/controllers/CourseController');
 
 // conrseContrller.index;
+router.get('/create', conrseContrller.create);
+router.post('/store', conrseContrller.store);
 router.get('/:slug', conrseContrller.show);
 
 module.exports = router;
